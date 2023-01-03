6abf74223e79f98a82d45403717299aff25317bb
Jyotsna B <b.jyotsna@in.bosch.com>
2023-01-03 03:52:00 +0000

    Update InventoryMasterBOService.java

--- src/main/java/com/bosch/whm/service/InventoryMasterBOService.java
+++ src/main/java/com/bosch/whm/service/InventoryMasterBOService.java
@@ -1,63 +1,88 @@
 
-/* Code developed by Sneha.S, Reviewed by Jyotsna
- *  Date: 21/12/2022 
- *  version 1.1
- */
-package com.bosch.whm.service;
-import com.bosch.whm.model.InventoryMasterBO;
+//21/12/2022 , file created by Sneha.s,reviewed by Jyotsna
+package com.bosch.whm;
+import java.lang.Object;
+import static org.junit.Assert.*; 
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.bosch.whm.model.Dimension;
 import com.bosch.whm.model.Product;
+import com.bosch.whm.service.InventoryMasterBOService; 
 
-public class InventoryMasterBOService {
-	//implement all methods of InventoryMasterBO
-	static InventoryMasterBO inventoryMasterBO;
-	//have to use ProductInBoundBO and implement it
-	public InventoryMasterBOService() {
-		// TODO Auto-generated constructor stub
-		inventoryMasterBO = new InventoryMasterBO();
+public class InventoryMasterBOServiceTest { 
 
-	}
-	public String addProductService(Product product)//epic 5
-	{
-		if(product!=null)
-		{
-			return inventoryMasterBO.addProduct(product);
-		}
+ //creating service instance
+ static InventoryMasterBOService inventoryMasterBOService;
+ static Product product;
+ @BeforeClass
+ public static void setUpBeforeClass() throws Exception {
+     inventoryMasterBOService = new InventoryMasterBOService();
+     product = new Product();
+ } 
 
-		else
-		{
-			return "null product";
-		}
+ @AfterClass
+ public static void tearDownAfterClass() throws Exception {
+     inventoryMasterBOService = null;
+ } 
 
-	}
-	public int increaseInventoryCountService()//epic 5
-	{	
-		return inventoryMasterBO.increaseInventoryCount();	
+ @Before
+ public void setUp() throws Exception {
+     Dimension dimension=new Dimension(5.0,6.0,7.0);
+		Product product=new Product(1,"mouse",10,dimension,5.0);
+ }
 
-	}
+ // Developed by Sasireha
+ @After
+ public void tearDown() throws Exception {
+ 	product = null;
+ } 
+ 
+ // Developed by Sasireha
+ //Testcase to check if product is successfully added.
+ @Test
+ public void testAddProductService() {
+	 Dimension dimension=new Dimension(5.0,6.0,7.0);
+		Product product=new Product(1,"mouse",10,dimension,5.0);
+ 	assertEquals("null product", inventoryMasterBOService.addProductService(null));
+ 	assertEquals("product added", inventoryMasterBOService.addProductService(product));
+ } 
+ 
+ // Developed by Sasireha
+ //returns total count of Inventory count after increasing
+ @Test
+ public void testIncreaseInventoryCountService() {
+     assertEquals(60, inventoryMasterBOService.increaseInventoryCountService());
+ } 
 
-	public int inventoryCountUpdateService()//epic 5
-	{
-		return inventoryMasterBO.inventoryCountUpdate();
+ // code developed by sneha
+ //returns total count of inventory
+ @Test
+ public void testInventoryCountUpdateService() {
+     assertEquals(60, inventoryMasterBOService.inventoryCountUpdateService());
+ } 
 
-	}
-	public int inventoryCountDamageService()//epic 5
-	{
-		return inventoryMasterBO.inventoryCountDamage();
+ // Developed by Sasireha
+ @Test
+ public void testInventoryCountDamageService() {
+     assertEquals(0, inventoryMasterBOService.inventoryCountDamageService());
+ } 
 
-	}
+ // Developed by Sasireha
+ //returns total count of inventory after decreasing
+ @Test
+ public void testDecreaseInventoryCountService() {
+     assertEquals(59, inventoryMasterBOService.decreaseInventoryCountService());
+ } 
 
+// @Test
+// public void testValidateDataService() {
+//     fail("Not yet implemented");
+// } 
 
-	public int decreaseInventoryCountService()//epic 5{	 
-
-	{
-		return inventoryMasterBO.decreaseInventoryCount();
-
-	}
-	public String validateDataService()//epic 5
-
-	{
-		inventoryMasterBO.validateData();
-		return null;
-
-	}
 }
+
